684ae109f947c78d46108b67d012323bcc651711
Dawn Smith <dawn.smith@camh.ca>
2023-08-15 13:26:44 -0400

    Fix issues with relative paths

--- local/report.age.R
+++ local/report.age.R
@@ -1,13 +1,20 @@
 #!/usr/bin/env Rscript
 
+get_exec_dir <- function() {
+    args <- commandArgs()
+    m <- regexpr("(?<=^--file=).+", args, perl=TRUE)
+    return(dirname(regmatches(args, m)))
+}
+
 report.age <- function(lh_file, rh_file, model_type = "") {
     library("kernlab")
-    source("../server/napr/R/load.mgh.R")
+    src_dir <- get_exec_dir()
+    source(file.path(src_dir, "../server/napr/R/load.mgh.R"))
 
     # Load pre-made models
-    load(file="../server/napr/data/rvmm.full.20161228.model.RData")
+    load(file=file.path(src_dir, "../server/napr/data/rvmm.full.20161228.model.RData"))
     if (model_type == "all") {
-        load(file="../server/napr/data/gausspr.full.20161228.model.RData")
+        load(file=file.path(src_dir, "../server/napr/data/gausspr.full.20161228.model.RData"))
         model.list = list(
             rvmm.full.20161228=rvmm.full.20161228.model,
             gausspr.full.20161228=gausspr.full.20161228.model
@@ -37,6 +44,9 @@ args = commandArgs(trailingOnly=TRUE)
 
 if (length(args) == 3) {
   model = args[3]
+} else if (length(args) < 2) {
+   print("Must provide lh and rh thickness fsaverage4 files.")
+   quit()
 } else {
   model = ""
 }
